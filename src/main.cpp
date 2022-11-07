7df90e786682a11335bc5e14bba750942191f30f
André Lehto <andre.r.lehto@gmail.com>
2022-11-07 16:42:18 +0100

    Implement Argument-parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+#include <QCommandLineParser>
+#include <QCoreApplication>
 int main(int argc, char* argv[]) {
+  QCoreApplication app(argc, argv);
+  QCoreApplication::setApplicationName("Hardware-finder");
+  QCoreApplication::setApplicationVersion("1.0");
+  QCommandLineParser parser;
+
+  parser.setApplicationDescription("CLI tool to read out hardware info.");
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+  QCommandLineOption targetFileOption(
+      QStringList() << "t"
+                    << "target-file",
+      QCoreApplication::translate(
+          "main", "Target <file-path> to write hardware-info to."),
+      QCoreApplication::translate("main", "file-path"));
+  parser.addOption(targetFileOption);
+
+  parser.process(app);
   return 0;
 }
