c5b12442b0bb8879affc7eab1e5b6496d967b5e5
André Lehto <andre.r.lehto@gmail.com>
2022-11-08 11:09:27 +0100

    Run processes asynchronous
    Rename class
    Make class instantiable

---
+++ src/wmi_api.cpp
@@ -0,0 +1,175 @@
+#include "wmi_api.h"
+#include <QDebug>
+
+WMIAPI::WMIAPI(QObject* parent) {
+  addTask(Win32_ComputerSystemProduct(std::string("IdentifyingNumber")),
+          Info::ProductID);
+  addTask(Win32_ComputerSystem(std::string("Name")), Info::MachineID);
+  addTask(Win32_OperatingSystem(std::string("Caption")), Info::OS);
+  addTask(Win32_OperatingSystem(std::string("Version")), Info::Version);
+  addTask(Win32_OperatingSystem(std::string("BuildNumber")), Info::Build);
+  addTask(Win32_ComputerSystem(std::string("TotalPhysicalMemory")), Info::Ram);
+  addTask(Win32_Processor(std::string("Name")), Info::CpuName);
+  addTask(Win32_VideoController(std::string("CurrentHorizontalResolution")),
+          Info::ResolutionHorizontal);
+  addTask(Win32_VideoController(std::string("CurrentVerticalResolution")),
+          Info::ResolutionVertical);
+  addTask(Win32_VideoController(std::string("MinRefreshRate")),
+          Info::RefreshRateMin);
+  addTask(Win32_VideoController(std::string("MaxRefreshRate")),
+          Info::RefreshRateMax);
+  addTask(Win32_VideoController(std::string("Name")), Info::GpuNames);
+  addTask(Win32_VideoController(std::string("DriverVersion")),
+          Info::GpuVersions);
+  addTask(Win32_VideoController(std::string("AdapterRam")), Info::GpuVram);
+}
+
+void WMIAPI::addTask(std::string target, Info type) {
+  Task task;
+  task.process = new QProcess();
+  task.target = QString::fromStdString(target);
+  task.type = type;
+  tasks.push_back(task);
+}
+
+void WMIAPI::runSearch() {
+  for (auto& task : tasks) {
+    task.process->start(
+        "C:/Windows/system32/WindowsPowerShell/v1.0/powershell.exe",
+        QString(task.target).split(" "));
+  }
+
+  QStringList gpuNames, gpuVersions, gpuVrams;
+
+  for (auto& task : tasks) {
+    task.process->waitForFinished();
+
+    switch (task.type) {
+      case Info::GpuNames:
+        gpuNames = QString(task.process->readAllStandardOutput())
+                       .remove("-")
+                       .remove("Name")
+                       .trimmed()
+                       .split("\n");
+        break;
+      case Info::GpuVersions:
+        gpuVersions = QString(task.process->readAllStandardOutput())
+                          .remove("-")
+                          .remove("DriverVersion")
+                          .trimmed()
+                          .split("\n");
+        break;
+      case Info::GpuVram:
+        gpuVrams = QString(task.process->readAllStandardOutput())
+                       .remove("-")
+                       .remove("AdapterRam")
+                       .trimmed()
+                       .split("\n");
+    }
+
+    QString result = QString(task.process->readAllStandardOutput())
+                         .trimmed()
+                         .split("\n")
+                         .last()
+                         .trimmed()
+                         .toLocal8Bit()
+                         .constData();
+
+    switch (task.type) {
+      case Info::ProductID:
+        _productID = result;
+        break;
+      case Info::MachineID:
+        _machineID = result;
+        break;
+      case Info::OS:
+        _os = result;
+        break;
+      case Info::Version:
+        _version = result;
+        break;
+      case Info::Build:
+        _build = result;
+        break;
+      case Info::Ram:
+        _ram = convertToGB(result);
+        break;
+      case Info::CpuName:
+        _cpuName = result;
+        break;
+      case Info::ResolutionVertical:
+        _resolutionVertical = result;
+        break;
+      case Info::ResolutionHorizontal:
+        _resolutionHorizontal = result;
+        break;
+      case Info::RefreshRateMin:
+        _refreshRateMin = result;
+        break;
+      case Info::RefreshRateMax:
+        _refreshRateMax = result;
+      default:
+        break;
+    }
+  }
+
+  for (qsizetype i = 0; i < gpuNames.size(); i++) {
+    GPU gpu;
+    gpu.name = gpuNames[i].toLocal8Bit().constData();
+    gpu.driverVersion = gpuVersions[i].toLocal8Bit();
+    gpu.VRAM = convertToGB(gpuVrams[i].toLocal8Bit());
+    _GPUs.push_back(gpu);
+  }
+}
+
+QString WMIAPI::productID() {
+  return _productID;
+}
+
+QString WMIAPI::machineID() {
+  return _machineID;
+}
+
+QString WMIAPI::os() {
+  return _os;
+}
+
+QString WMIAPI::version() {
+  return _version;
+}
+
+QString WMIAPI::build() {
+  return _build;
+}
+
+QString WMIAPI::RAM() {
+  return _ram;
+}
+
+QString WMIAPI::CPUName() {
+  return _cpuName.split(" ").first(3).join(" ").simplified();
+}
+
+QString WMIAPI::CPUClockSpeed() {
+  return _cpuName.split(" ").last().simplified();
+}
+
+QString WMIAPI::resolution() {
+  return QString(_resolutionHorizontal + "x" + _resolutionVertical);
+}
+
+QString WMIAPI::refreshRate() {
+  return QString(_refreshRateMin + "-" + _refreshRateMax);
+}
+
+QVector<WMIAPI::GPU> WMIAPI::GPUs() {
+  return _GPUs;
+}
+
+QString WMIAPI::convertToGB(QString string) {
+  return QString::number(
+             QString(string).simplified().split(" ").last().toDouble() /
+                 1000000000,
+             'g', 3)
+      .append(" GB");
+}
---
+++ src/wmi_api.h
@@ -0,0 +1,71 @@
+#include <QProcess>
+
+#define Win32_ComputerSystemProduct(target)                                   \
+  "Get-CimInstance -ClassName Win32_ComputerSystemProduct | Select-Object " + \
+      target
+#define Win32_ComputerSystem(target) \
+  "Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object " + target
+#define Win32_OperatingSystem(target) \
+  "Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object " + target
+#define Win32_Processor(target) \
+  "Get-CimInstance -ClassName Win32_Processor | Select-Object " + target
+#define Win32_VideoController(target) \
+  "Get-CimInstance -ClassName Win32_VideoController | Select-Object " + target
+
+class WMIAPI : public QObject {
+  Q_OBJECT
+
+ public:
+  explicit WMIAPI(QObject* parent = nullptr);
+
+  struct GPU {
+    QString name, driverVersion, VRAM;
+  };
+
+  void runSearch();
+
+  QString productID();
+  QString machineID();
+  QString os();
+  QString version();
+  QString build();
+  QString RAM();
+  QString CPUName();
+  QString CPUClockSpeed();
+  QString resolution();
+  QString refreshRate();
+  QVector<GPU> GPUs();
+
+ private:
+  enum class Info {
+    ProductID,
+    MachineID,
+    OS,
+    Version,
+    Build,
+    Ram,
+    CpuName,
+    ResolutionVertical,
+    ResolutionHorizontal,
+    RefreshRateMin,
+    RefreshRateMax,
+    GpuNames,
+    GpuVersions,
+    GpuVram
+  };
+
+  struct Task {
+    QProcess* process;
+    QString target;
+    Info type;
+  };
+
+  QString _productID, _machineID, _os, _version, _build, _ram, _cpuName,
+      _cpuClockSpeed, _resolutionVertical, _resolutionHorizontal,
+      _refreshRateMin, _refreshRateMax;
+  QVector<GPU> _GPUs;
+  QVector<Task> tasks;
+
+  void addTask(std::string target, Info info);
+  QString convertToGB(QString bytes);
+};
